2ed22afc21c4001a41c527f4e802b8218b94ea02
Ilya Savinkin <iasavinkin@gmail.com>
2025-01-23 16:07:11 +0300

    added get,delete,update logic for reservation

--- app/crud/reservation.py
+++ app/crud/reservation.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from typing import Optional
 
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy import select, and_
@@ -11,19 +12,22 @@ class CRUDReservation(CRUDBase):
 
     async def get_reservations_at_the_same_time(
             self,
+            *,
             from_reserve: datetime,
             to_reserve: datetime,
             room_id: int,
+            reservation_id: Optional[int] = None,
             session: AsyncSession) -> list[Reservation]:
-        reservations = await session.execute(
-            select(Reservation).where(
+        select_stmt = select(Reservation).where(
                 Reservation.room_id == room_id,
                 and_(
                     from_reserve <= Reservation.to_reserve,
                     to_reserve >= Reservation.from_reserve
                 )
-            )
         )
+        if reservation_id is not None:
+            select_stmt = select_stmt.where(Reservation.id != reservation_id)
+        reservations = await session.execute(select_stmt)
         reservations = reservations.scalars().all()
         return reservations
 
--- app/schemas/reservation.py
+++ app/schemas/reservation.py
@@ -1,11 +1,16 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 
-from pydantic import BaseModel, field_validator, model_validator
+from pydantic import BaseModel, field_validator, model_validator, Field
+
+FROM_TIME = (datetime.now() + timedelta(minutes=10)
+             ).isoformat(sep=' ', timespec='minutes')
+TO_TIME = (datetime.now() + timedelta(hours=1)
+           ).isoformat(sep=' ', timespec='minutes')
 
 
 class ReservationBase(BaseModel):
-    from_reserve: datetime
-    to_reserve: datetime
+    from_reserve: datetime = Field(..., example=FROM_TIME)
+    to_reserve: datetime = Field(..., example=TO_TIME)
 
 
 class ReservationUpdate(ReservationBase):
@@ -22,6 +27,9 @@ class ReservationUpdate(ReservationBase):
             raise ValueError('reservation end must be after start')
         return self
 
+    class Config:
+        extra = 'forbid'
+
 
 class ReservationCreate(ReservationUpdate):
     room_id: int
@@ -33,3 +41,13 @@ class ReservationDB(ReservationBase):
 
     class Config:
         orm_mode = True
+
+    @staticmethod
+    def _format_datetime(value: datetime) -> datetime:
+        if isinstance(value, datetime):
+            return value.isoformat(sep=' ', timespec='minutes')
+        return value
+
+    @field_validator('from_reserve', 'to_reserve')
+    def format_datetime(cls, value):
+        return cls._format_datetime(value)
